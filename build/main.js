120d6cdfda9ec8057aa20e1ebdc15235929c63de
koreanwglasses <koreanwglasses@gmail.com>
2021-03-23 11:22:43 -0400

    build

---
+++ build/main.js
@@ -0,0 +1,16 @@
+(() => {
+  "use strict";
+  const e = React,
+    t = ReactDOM;
+  t.render(
+    e.createElement(function () {
+      return e.createElement(
+        "div",
+        { className: "_23OuAh4L4HxSCYQjGanulb" },
+        "Hello World"
+      );
+    }, null),
+    document.getElementById("react-root")
+  );
+})();
+//# sourceMappingURL=main.js.map
