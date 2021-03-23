b9573b0bf5c6f8991b12040e5800c29c282084fc
koreanwglasses <koreanwglasses@gmail.com>
2021-03-23 16:56:39 -0400

    forward messages from discord to frontend

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,29 @@
 import * as React from "react";
 import styles from "./app.module.css";
 import { fetch2 } from "./utils";
+import { io } from "socket.io-client";
+import { Message } from "discord.js";
 
 export function App() {
-  const [data, setData] = React.useState<{ message: string }>();
+  const [messages, addMessage] = React.useReducer(
+    (state: Message[], newMessage: Message) => {
+      return [...state, newMessage];
+    },
+    [] as Message[]
+  );
 
   React.useEffect(() => {
-    (async () => {
-      const result = await fetch2("/api/test");
-      setData(await result.json());
-    })();
+    const socket = io("http://localhost:9000");
+    socket.on("discord-message", (message: Message) => {
+      addMessage(message);
+    });
   }, []);
 
-  return <div className={styles.container}>{data?.message}</div>;
+  return (
+    <div className={styles.container}>
+      {messages.map((message) => (
+        <p key={message.id}>{message.content}</p>
+      ))}
+    </div>
+  );
 }
